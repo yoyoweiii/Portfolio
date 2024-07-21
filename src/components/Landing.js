dd544918f06f40affcb9ef5ef796d727eccbac43
yoyoweiii <fdsa6598@#gmail.com>
2024-07-20 21:45:37 -0500

    testing on deploy

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,7 +7,7 @@ function Landing() {
     <section id="landing">
       <div className="landing-content">
         <img src={avatar} alt="Avatar" className="avatar" />
-        <h1>Your Name</h1>
+        <h1>Yu-Wei (Kevin) Hsu</h1>
         <p>Hello! My name is Yu-Wei(Kevin), a recent graduate from Auburn University with a Bachelor's in Computer Science and a Master's in Management Information Systems.</p>
         <p>I am seeking software developer and data analyst positions. My education has provided me with a strong foundation in programming, data mining, and analytics.</p>
         <p>I have hands-on experience from projects like GlassCell, an award-winning accessibility hack that addresses emergency situations using Python, JavaScript, and Django, and an interactive bot that delivers real-time traffic updates using Python, OpenStreetMap, and Flask.</p>
