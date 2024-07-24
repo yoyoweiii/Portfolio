6fa3af168031f0df8536ed3726b6b41e2602cdba
yoyoweiii <fdsa6598@#gmail.com>
2024-07-23 20:18:49 -0500

    Testing 3

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -11,8 +11,7 @@ function Landing() {
         <p>Hello! My name is Yu-Wei(Kevin), a recent graduate from Auburn University with a Bachelor's in Computer Science and a Master's in Management Information Systems.</p>
         <p>I am seeking software developer and data analyst positions. My education has provided me with a strong foundation in programming, data mining, and analytics.</p>
         <p>I have hands-on experience from projects like GlassCell, an award-winning accessibility hack that addresses emergency situations using Python, JavaScript, and Django, and an interactive bot that delivers real-time traffic updates using Python, OpenStreetMap, and Flask.</p>
-        <p> I am confident that my technical skills and innovative problem-solving approach will contribute significantly to your organization.</p>
-        <p>Thank you for your time and consideration.</p>
+
       </div>
     </section>
   );
