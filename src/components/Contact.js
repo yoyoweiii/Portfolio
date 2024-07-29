c5312b09c46a423d7f57f75d1ba5e5f1765c5300
yoyoweiii <fdsa6598@#gmail.com>
2024-07-28 22:33:58 -0500

    contact function added and work

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,63 @@
-import React from 'react';
-import './Contact.css';
+import React, { useState } from "react";
+import "./Contact.css";
+import { useForm, ValidationError } from "@formspree/react";
 
 function Contact() {
-  const handleSubmit = (e) => {
+  const [state, handleSubmit] = useForm("mjkbylgw");
+  const [message, setMessage] = useState("");
+
+  const handleFormSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission logic here
+    const additionalSentence =
+      "This message was sent from portfolio in Github.io.";
+    const fullMessage =
+      `Dear Kevin, \n\n` + `${message}\n\n${additionalSentence}`;
+
+    // Create a new FormData object and append the necessary fields
+    const formData = new FormData(e.target);
+    formData.set("message", fullMessage);
+
+    // Convert FormData to an object to pass it to handleSubmit
+    const formObject = {};
+    formData.forEach((value, key) => {
+      formObject[key] = value;
+    });
+
+    handleSubmit(formObject);
   };
 
+  if (state.succeeded) {
+    return (
+      <div>
+        <p>Thanks for contacting!</p>
+        <p>Let's keep in touch</p>
+      </div>
+    );
+  }
+
   return (
-    <section id="contact">
-      <h2>Contact Me</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" name="name" required />
-        </label>
-        <label>
-          Email:
-          <input type="email" name="email" required />
-        </label>
-        <label>
-          Message:
-          <textarea name="message" required></textarea>
-        </label>
-        <button type="submit">Send</button>
-      </form>
-    </section>
+    <form onSubmit={handleFormSubmit}>
+      <label htmlFor="email">Email Address:</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        placeholder="example@mail.com"
+      />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <label>Content:</label>
+      <textarea
+        id="message"
+        name="message"
+        placeholder="Tell me what you feel"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
   );
 }
 
